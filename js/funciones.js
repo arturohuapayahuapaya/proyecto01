ec1e49219ce6a381db34b0b524c036c3d59f525e
arturohuapayahuapaya <118744548+arturohuapayahuapaya@users.noreply.github.com>
2022-11-21 09:06:58 -0500

    Subiendo funciones.js
    Archivo js de pagina1.html

---
+++ js/funciones.js
@@ -0,0 +1,19 @@
+/* Funciones incorporadas */
+
+alert("Bienvenido");
+
+/* 
+Datos de entrada
+variables 
+*/
+
+var num1 = 12;
+var num2 = 10;
+
+/* proceso */
+var total = num1 + num2;
+
+/* salida */
+
+alert("La suma es: " + total);
+
